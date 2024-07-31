ccdd1d24f01e66df0ee8181c26e1eded314b6566
2077collective <dimmstreet@gmail.com>
2024-07-31 17:41:10 +0100

    chore(update): add eip-4844 article

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+    site: "https://eips.wiki",
 	integrations: [
 		starlight({
 			title: '2077\'s EIP Wiki',
